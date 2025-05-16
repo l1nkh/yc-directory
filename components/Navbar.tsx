dd93fe8fd88264bad914af16be40dcde915e6273
l1nkh <diogo.m.henriques98@gmail.com>
2025-05-16 01:01:56 +0100

    Add project

---
+++ components/Navbar.tsx
@@ -0,0 +1,61 @@
+import React from 'react';
+import Image from 'next/image';
+import Link from 'next/link';
+import { auth, signOut, signIn } from '@/auth';
+import { BadgePlus, LogOut } from 'lucide-react';
+import { Avatar, AvatarFallback, AvatarImage } from './ui/avatar';
+
+const Navbar = async () => {
+    const session = await auth();
+    return (
+        <header className='px-5 py-3 bg-white shadow-sm font-work-sans'>
+            <nav className='flex items-center justify-between'>
+                <Link href="/">
+                    <img src="/logo.png" alt="logo" width={144} height={30} />
+                </Link>
+
+                <div className='flex items-center gap-5 text-black'>
+                    {session && session?.user ? (
+                        <>
+                            <Link href="/startup/create">
+                                <span className='max-sm:hidden'>Create</span>
+                                <BadgePlus className='size-6 sm:hidden' />
+                            </Link>
+
+                            <form action={async () => {
+                                "use server";
+                                await signOut({ redirectTo: '/' })
+                            }}>
+                                <button type='submit'>
+                                    <span className='max-sm:hidden'>Logout</span>
+                                </button>
+                                <LogOut className='size-6 relative -top-[.75rem] sm:hidden text-red-500' />
+                            </form>
+
+                            <Link href={`/user/${session?.id}`}>
+                                <Avatar className='size-10'>
+                                    <AvatarImage
+                                        src={session?.user?.image || "https://placehold.co/128x128"}
+                                        alt={session?.user?.name || session?.user?.name}
+                                    />
+                                    <AvatarFallback>AV</AvatarFallback>
+                                </Avatar>
+                            </Link>
+                        </>
+                    ) : (
+                        <>
+                            <form action={async () => {
+                                "use server";
+                                await signIn('github')
+                            }}>
+                                <button type='submit'>Login</button>
+                            </form>
+                        </>
+                    )}
+                </div>
+            </nav>
+        </header>
+    )
+}
+
+export default Navbar
